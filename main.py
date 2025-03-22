67ea36de175e315cb850ebecfb51f152b5d07107
김다엘 <daelkdev@gmail.com>
2025-03-22 18:50:01 +0900

    move right iframe and get menu page

--- main.py
+++ main.py
@@ -2,8 +2,10 @@ import time
 from urllib.parse import quote
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
-from utils.frame_switch import switch_left
+from utils.frame_switch import switch_left, switch_right
 
 options = webdriver.ChromeOptions()
 options.add_argument('user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3')
@@ -12,7 +14,7 @@ driver = webdriver.Chrome(options=options)
 
 driver.implicitly_wait(time_to_wait=3)
 
-search_term = "한신포차"
+search_term = "이백장돈까스"
 encoded_term = quote(search_term)
 URL = f"https://map.naver.com/v5/search/{encoded_term}"
 
@@ -25,22 +27,50 @@ collected_names = []
 while True:
     scroll_container = driver.find_element(By.ID, "_pcmap_list_scroll_container")
     prev_height = driver.execute_script("return arguments[0].scrollHeight", scroll_container)
-
+    
+    # Scroll to the bottom of the list
     while True:
         driver.execute_script("arguments[0].scrollTop = arguments[0].scrollHeight", scroll_container)
-        time.sleep(1)
+        time.sleep(0.5)
         new_height = driver.execute_script("return arguments[0].scrollHeight", scroll_container)
         
         if new_height == prev_height:
             break
         prev_height = new_height
 
-    elements = driver.find_elements(By.CSS_SELECTOR, "li.UEzoS.rTjJo span.TYaxT")
-    current_names = [el.text for el in elements]
+    name_elements = driver.find_elements(By.CSS_SELECTOR, "li.UEzoS.rTjJo span.TYaxT")
+    current_names = [name.text for name in name_elements]
     
     for name in current_names:
         if name not in collected_names:
             collected_names.append(name)
+            
+    side_bar_button_elements = driver.find_elements(By.CSS_SELECTOR, "li.UEzoS.rTjJo a.tzwk0")
+    side_bar_buttons = [button for button in side_bar_button_elements]
+    
+    for button in side_bar_buttons:
+        button.click()
+        
+        switch_right(driver)
+        
+        menu_links = driver.find_elements(By.CSS_SELECTOR, "a.tpj9w._tab-menu")
+        for link in menu_links:
+            try:
+                span = link.find_element(By.CSS_SELECTOR, "span.veBoZ")
+                if span.text.strip() == "메뉴":
+                    link.click()
+                    
+                    more_menu_button = driver.find_element(By.CSS_SELECTOR, "a.fvwqf")
+                    more_menu_button.click()
+                    
+                    break
+            except Exception as e:
+                print("예외 발생:", e)
+                continue
+        
+        time.sleep(1)
+        
+        switch_left(driver)
 
     try:
         list_buttons = driver.find_elements(By.CSS_SELECTOR, "div.zRM9F a[target='_self']")
@@ -55,7 +85,7 @@ while True:
             break
         else:
             next_list_button.click()
-            time.sleep(1)
+            time.sleep(0.5)
     
     except Exception as e:
         print("get error while finding next button:", e)
--- utils/frame_switch.py
+++ utils/frame_switch.py
@@ -1,13 +1,11 @@
 from selenium.webdriver.common.by import By
 
 def switch_left(driver):
-    ############## iframe으로 왼쪽 포커스 맞추기 ##############
     driver.switch_to.parent_frame()
-    iframe = driver.find_element(By.XPATH,'//*[@id="searchIframe"]')
-    driver.switch_to.frame(iframe)
+    left_iframe = driver.find_element(By.XPATH,'//*[@id="searchIframe"]')
+    driver.switch_to.frame(left_iframe)
     
 def switch_right(driver):
-    ############## iframe으로 오른쪽 포커스 맞추기 ##############
     driver.switch_to.parent_frame()
-    iframe = driver.find_element(By.XPATH,'//*[@id="entryIframe"]')
-    driver.switch_to.frame(iframe)
+    right_iframe = driver.find_element(By.XPATH,'//*[@id="entryIframe"]')
+    driver.switch_to.frame(right_iframe)
